c022a13081f3ac3b5f1ee70b9a3cd34dd457ea45
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2019-11-08 11:37:11 +0300

    User registration

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -4,7 +4,7 @@
     {
         public function __construct()
         {
-
+            $this->userModel = $this->model('User');
         }
 
         public function register()
@@ -62,12 +62,21 @@
                 // Make sure errors are empty
                 if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) &&
                     empty($data['confirm_password_err'])) {
-                    die('SUCCESS');
+                    // Validated
+
+                    // HAsh Password
+                    $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                    //Register User
+                    if($this->userModel->register($data)){
+                        redirect('users/login');
+                    } else {
+                        die('Something went wrong');
+                    }
                 } else {
                     // Load view with errors
                     $this->view('users/register', $data);
                 }
-
 
             } else {
                 $data = [
